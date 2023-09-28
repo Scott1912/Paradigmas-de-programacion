7b401c230de464bb57d8fcf949d3fe39ae6f4547
Scott1912 <scott1912@gmail.com>
2023-09-28 11:53:48 -0600

    08 completado

--- 08_ObjetosIntro.py
+++ 08_ObjetosIntro.py
@@ -40,5 +40,141 @@ class Llanta:
     # parámetros de entrada = default
     #========================================
     def __init__(mi,radio=50,ancho=30,presión=1.5):
-        pass
+        #vairable de la estructura comleta Llanta
+        Llanta.cuenta += 1
+        mi.radio = radio
+        mi.radio = ancho
+        mi.presión = presión
+
+#===========================
+# Objetos (instaciados)
+#===========================
+llanta1 = Llanta(50,30,1.5)
+llanta2 = Llanta(presión=1.2)
+llanta3 = Llanta()
+llanta4 = Llanta(40,30,1.6)
+
+#=====================================
+# Objeto que contiene otros objetos
+#=====================================
+class Coche:
+    def __init__(mi,ll1,ll2,ll3,ll4):
+        mi.llanta1 = ll1
+        mi.llanta2 = ll2
+        mi.llanta3 = ll3
+        mi.llanta4 = ll4
+
+micoche = Coche(llanta1,llanta2,llanta3,llanta4)
+
+print("Total de llantas: ",Llanta.cuenta) #Variable global de la clase
+print("Presión de la llanta 4 = ",llanta4.presión) # Presión de la llanta 4
+print("Radio de la llanta 4 = ",llantaradio)
+print("Radio de la llanta 3 = ",llanta3.radio)
+print("Presión de la llanta 1 de mi coche = ", micoche.llanta1.presión)
+
+#=============================
+# Encapsulamiento
+#=============================
+
+#=====================================================================
+# Uso de la función de python property para poner y obtener atributos
+#=====================================================================
+class Estudiante:
+    def __init__(mi):
+        mi.__nombre = ''
+    def ponerme_nombre(mi, nombre):
+        print('se llamó a ponerme_nombre')
+        mi.__nombre = nombre
+    def obtener_nombre(mi):
+        print('se llamó a obtener_nombre')
+        return mi.__nombre
+    nombre = property(obtener_nombre,ponerme_nombre)
+
+#=====================================
+# Creat objeto estudiante sin nombre
+#=====================================
+estudiante = Estudiante()
+
+#============================================================================
+# Ponerle nombre usando la propiedad nombre y el método ponerme_nombre
+# (sin tener que llamar explícitamente la función)
+#============================================================================
+estudiante.nombre = "Diego"
+
+#===================================================================
+# Obtener el nombre con el método obtener_nombre
+# __nombre es una variable encapsulada (no visible desde fuera)
+# (sin tener que llamar explícitamente a la función obtener_nombre)
+#===================================================================
+print(estudiante.nombre)
+
+# Esto no funciona
+# print(estudiante.__nombre)
+
+#========================
+# Herencia de clases
+#========================
+class Cuadrilatero:
+    def __init__(mi, a, b, c, d):
+        mi.lado1 = a
+        mi.lado2 = b
+        mi.lado3 = c
+        mi.lado4 = d
+
+    def perimetro(mi):
+        p = mi.lado1 + mi.lado2 + mi.lado3 + mi.lado4
+        print("perimetro=",p)
+        return p
+
+#=====================================
+# Su hijo rectángulo
+# Rectángulo es hijo de Cuadrilátero
+# Rectángulo(Cuadrilatero)
+#=====================================
+class Rectangulo(Cuadrilatero):
+    def __init__(self, a, b):
+        #===========================
+        # Constructor de su madre
+        #===========================
+        super().__init__(a, b, a, b)
+
+#=====================
+# Su nieto Cuadrado
+# Hijo de Rectángulo
+#=====================
+class Cuadrado(Rectangulo):
+    def __init__(self, a):
+        super().__init__(a,a)
+
+    def area(self):
+        area = self.lado1**2
+        return area
+
+    #def perimetro(self):
+    #    p = 4.0*self.lado1
+    #    print("perimetro =",p)
+    #    return p
+
+#======================
+# Crear un cuadrado
+#======================
+cuadrado1 = Cuadrado(5)
+
+#==========================================================
+# Llamar al método perímetro de su abuelo Cuadrilatero
+#==========================================================
+perimetro1 = cuadrado1.perimetro()
+
+#================================
+# Llamar a su propio método área
+#================================
+area1 = cuadrado1.area()
+
+print("Perímetro = ", perimetro1)
+print("Área = ", area1)
+
+#==================================================================
+# Sobre-escribir un método de su madre o abuela o tatarabuela...
+# Es volver a definir una función ya existente
+#==================================================================
 
