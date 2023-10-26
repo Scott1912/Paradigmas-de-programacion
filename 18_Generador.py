f7f47b3d0a380e82d824eeb40099d3ff2bf55a59
Scott1912 <scott1912c@gmail.com>
2023-10-26 15:03:45 -0600

    se hizo 17 y 18

---
+++ 18_Generador.py
@@ -0,0 +1,30 @@
+#===============================
+# Chávez Mendoza Bryan Alexis
+#===============================
+# Paradigmas de Programación
+# Matemática Algorítmica
+# ESFM IPN Octubre 2023
+#===============================
+
+#========================================
+# yield transforma la fución a iterador
+#========================================
+def migenerador():
+    print("Primero")
+    yield 10
+    print("Segundo")
+    yield "20"
+    print("Tercero")
+    yield "hola"
+
+#=========================
+# gen es un iterador
+#=========================
+gen = migenerador()
+val1 = next(gen)
+print(val1)
+val2 = next(gen)
+print(val2)
+val3 = next(gen)
+print(val3)
+
